ab6f9f724e8f520004ce8ee443e817383cb1abac
Heiko Neuberger <42500448+heikone@users.noreply.github.com>
2024-09-03 21:13:21 +0200

    value converted to string
    some parameters (like power_mng.soc_strategy) only accept values as a string

--- rct_ha_call.py
+++ rct_ha_call.py
@@ -4,6 +4,8 @@ def rct_ha_call(action='set', parameter='power_mng.soc_max', value=0.97, host='1
 
     from rct import get_value, set_value
 
+    value = str(value) # some parameters (like power_mng.soc_strategy) only accept values as a string
+
     output = set_value(parameter=parameter, value=value, host=host)
     print(output)
     log.info(f"rct_ha_call: {output}")
